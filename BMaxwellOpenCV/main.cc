d785757e0c76e9cef89b22da648833929185ff3b
LazarusCore <benson@svm.nz>
2017-05-15 20:42:06 +1200

    Added Hough Circles

--- BMaxwellOpenCV/main.cc
+++ BMaxwellOpenCV/main.cc
@@ -1,20 +1,23 @@
 //Include
 
 #include <opencv2\opencv.hpp>
-#include <iostream>
 #include <math.h>
+#include <stdio.h>
+#include <vector>
 
 //Benson Maxwell: 01504282
 
 //Namespaces
 
 using namespace cv;
+using namespace std;
 
 //Function prototypes
-void edgeDet(Mat input, int width, int height, float sigma);											//Canny style edge detection
-void imagePro(int width, int height);																	//Sobel filter
-void findEdge(int rowShift, int colShift, int row, int col, int direction, int width, int height);	//Find the edge provvided by the Sobel
-void nonMaxima(int rowShift, int colShift, int row, int col, int direction, int width, int height);	//Remove nonMax edges
+void edgeDet(Mat& input, int width, int height, float sigma);											//Canny style edge detection
+void imagePro(int width, int height);																	//Canny Filter filter
+void findEdge(int rowShift, int colShift, int row, int col, int direction, int width, int height);		//Find the edge provvided by the Sobel
+void nonMaxima(int rowShift, int colShift, int row, int col, int direction, int width, int height);		//Remove nonMax edges
+void houghCircle(Mat& original, int width, int height);
 
 //Macros for greyscale
 #define pixel(image ,y, x, step, channels) ( (uchar*) ((( image).data ) + (y) *(step) ) ) [( x)*(channels)]
@@ -27,23 +30,26 @@ int imageX, imageY;												// Image Resoltion
 float sigma = 1.0;												// Canny Filter Standard deviation
 int gradX;														// Sum of Sobel mask products values in the x direction
 int gradY;														// Sum of Sobel mask products values in the y direction
-int GxMask[3][3] = { {-1,0,1}, {-2,0,2}, {-1,0,-1} };			// Sobel mask in the x direction
-int GyMask[3][3] = { {1,2,1}, {0,0,0}, {-1,-2,-1} };			// Sobel mask in the y direction
+int GxMask[3][3] = { {-3,0,3}, {-10,0,10}, {-3,0,-3} };			// Sobel mask in the x direction
+int GyMask[3][3] = { {3,10,3}, {0,0,0}, {-3,-10,-3} };			// Sobel mask in the y direction
 int edgeDirection[3072][1728];									// Stores the edge direction of each pixel
-double pixGradient[3072][1728];									// Stores the gradient strength of each pixel
+int pixGradient[3072][1728];									// Stores the gradient strength of each pixel
 Mat impro_image(1728, 3072, CV_8UC1);							// Mat file for Sobel Operator
 Mat grey_image(1728, 3072, CV_8UC1);							// Mat file for Gray scale Operator
 Mat blur_image(1728, 3072, CV_8UC1);							// Gaussian Blur result
-float thresLow = 0.5;											// Lower Threshold
-float thresHigh = 0.7;											// Upper Threshold
+Mat hough_image(1728, 3072, CV_8UC1);							// Hough result
+float thresLow = 0.100;											// Lower Threshold
+float thresHigh = 0.5;											// Upper Threshold
 int step = impro_image.step;
 int channels = impro_image.channels();
-
+int radMin = 130;												// Min radius to look for
+int radMax = 195;												// Max Radius to look for
+vector<Vec3i> houghAcc;											// Hough Accumulator (Row, Col, Radius)
 
 
 int main(int argc, char* args[]) {
 
-	Mat	src_image = imread(FILE_NAME, CV_LOAD_IMAGE_COLOR);		// Loads the image1.jpg into the variable src_image
+	Mat src_image = imread(FILE_NAME, CV_LOAD_IMAGE_COLOR);		// Loads the image1.jpg into the variable src_image
 	imageX = src_image.size().width;
 	imageY = src_image.size().height;
 
@@ -52,11 +58,11 @@ int main(int argc, char* args[]) {
 
 	imwrite("../../BMaxwellOpenCV/Grey_Image.jpg", grey_image);
 	
-	namedWindow("Image", CV_WINDOW_NORMAL);						// Opens images for comparison
-	namedWindow("Grey Image", CV_WINDOW_NORMAL);
+	//namedWindow("Image", CV_WINDOW_NORMAL);						// Opens images for comparison
+	//namedWindow("Grey Image", CV_WINDOW_NORMAL);
 
-	imshow("Image", src_image);
-	imshow("Grey Image", grey_image);
+	//imshow("Image", src_image);
+	//imshow("Grey Image", grey_image);
 
 	edgeDet(grey_image, imageX, imageY, sigma); //Runs the Edge Detection algorithm
 
@@ -67,22 +73,25 @@ int main(int argc, char* args[]) {
 	return 0;
 }
 
-void edgeDet(Mat input, int width, int height, float sigma){
+void edgeDet(Mat &input, int width, int height, float sigma){
 
 	Mat kernel = (Mat_<double>(5, 5) << 1, 4, 6, 4, 1, 4, 16, 24, 16, 4, 6, 24, 36, 24, 6, 4, 16, 24, 16, 4, 1, 4, 6, 4, 1);	//Gaussian Kernel
-	kernel = kernel / 256;										//Normalised
+	kernel = kernel / 350;										//Normalised
 
 	filter2D(input, blur_image, -1, kernel, Point(-1, -1), 0);	//Convolve for Gaussian Blur creation
 
 	imwrite("../../BMaxwellOpenCV/Blur_Image.jpg", blur_image);
-	namedWindow("Blur Image", CV_WINDOW_NORMAL);
-	imshow("Blur Image", blur_image);
+	//namedWindow("Blur Image", CV_WINDOW_NORMAL);
+	//imshow("Blur Image", blur_image);
 	
 	imagePro(width, height);	//Sobel Function
 
+
 	imwrite("../../BMaxwellOpenCV/impro_Image.jpg", impro_image);
-	namedWindow("Canny Image", CV_WINDOW_NORMAL);
-	imshow("Canny Image", impro_image);
+	//namedWindow("Canny Image", CV_WINDOW_NORMAL);
+	//imshow("Canny Image", impro_image);
+
+	houghCircle(input, width, height);
 
 	return ;
 
@@ -174,7 +183,7 @@ void imagePro(int width, int height){
 	// Suppress any pixels not changed by the edge tracing
 	for (row = 0; row < height; row++) {
 		for (col = 0; col < width; col++) {
-			// If a pixel's grayValue is not black or white make it black
+			// If a pixel's grayValue is not black or white make it white
 			if ((pixel(impro_image, row, col, step, channels) != 255) && (pixel(impro_image, row, col, step, channels) != 0)) {
 				pixel(impro_image, row, col, step, channels) = 0;
 			}
@@ -247,7 +256,7 @@ void findEdge(int rowShift, int colShift, int row, int col, int direction, int w
 	//Determine if edge directions and gradient strengths correspond
 	while ((edgeDirection[newCol][newRow] == direction) && !edgeEnd && (pixGradient[newCol][newRow] > thresLow)) {
 		
-		//Set the pixel to white
+		//Set the pixel to black
 		pixel(impro_image, row, col, step, channels) = 255;							//need to pass this back and forward somehow   //uncomment this line when error is solved
 
 		if (colShift < 0) {
@@ -281,7 +290,7 @@ void nonMaxima(int rowShift, int colShift, int row, int col, int direction, int 
 	int newRow = 0;
 	int newCol = 0;
 	bool edgeEnd = false;
-	float nonMax[3072][3];													// Temporarily stores gradients and positions of pixels in parallel edges
+	int nonMax[3072][3];															// Temporarily stores gradients and positions of pixels in parallel edges
 	int pixelCount = 0;																// Stores the number of pixels in parallel edges
 	int count;
 	int max[3];																		// Maximum point in a wide edge
@@ -414,4 +423,77 @@ void nonMaxima(int rowShift, int colShift, int row, int col, int direction, int 
 	}
 
 	return ;
+}
+
+
+void houghCircle(Mat &original, int width, int height) {
+
+	int i, j, k;
+	int x = 0;
+	int y = 0;
+	Vec3i tempVec;
+
+
+	//For each possible radius, go through the impro_image and create the hough space.
+	for (k = radMin; k <= radMax; k++) 
+	{
+		hough_image = Scalar(0);
+
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				//If the pixel is black (edge) then carry find the circles that it could fall on
+				if (pixel(impro_image, i, j, step, channels) == 255) 
+				{	
+					for (y = -k; ( i + y ) < (i + k); y++)
+					{
+						for (x = -k; (j + x) < (j + k); x++) 
+						{
+							if ((i + y < 0) || (j + x < 0))
+							{
+								if (pow(y, 2) + pow(x, 2) == pow(k, 2)) 
+								{
+									//Check if we have intensity overflow on the hough image.
+									if (pixel(hough_image, (i + y), (j + x), step, channels) == 255)
+									{
+										break;
+									}
+									else
+									{
+										//increase the intensity of that pixel by 1
+										pixel(hough_image, (i + y), (j + x), step, channels) += 1;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		//check the map for maxima, and place maxima into the hough accumulator
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				if (pixel(hough_image, i, j, step, channels) > 10)
+				{
+					tempVec[0] = i;													//Row
+					tempVec[1] = j;													//Col
+					tempVec[2] = k;													//Radius
+					houghAcc.push_back(tempVec);
+					
+				}
+			}
+		}
+	}
+
+	for (i = 0; i < houghAcc.size(); i++)
+	{
+		circle(original, Point(houghAcc[i][0], houghAcc[i][1]), houghAcc[i][2], Scalar(0, 255, 255), 1, 8, 0);
+	}
+
+	namedWindow("Image", CV_WINDOW_NORMAL);
+	imshow("Image", original);
+
 }
